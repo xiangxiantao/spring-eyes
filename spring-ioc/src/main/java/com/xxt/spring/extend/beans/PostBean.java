ae453f8123cbebc745f418dac85af0f4d708328e
haytt.xiang <haytt.xiang@xgimi.com>
2020-09-23 09:11:28 +0800

    beanPostProcessor

--- spring-ioc/src/main/java/com/xxt/spring/extend/beans/PostBean.java
+++ spring-ioc/src/main/java/com/xxt/spring/extend/beans/PostBean.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
 /**
  * @description: com.xxt.spring.extend.beans
  * @author: haytt.xiang
@@ -15,12 +17,26 @@ import org.springframework.stereotype.Component;
 @Order(value = 1)//value越小越先执行，默认最后执行
 public class PostBean implements BeanPostProcessor  {
 
+    /**
+     * 这个方法在bean实例化之后，执行初始化回调方法之前执行
+     * @param bean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         System.out.println(beanName + "before init");
         return bean;
     }
 
+    /**
+     * 这个方法在bean执行完初始化回调之后执行
+     * @param bean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         System.out.println(beanName + "after init");
